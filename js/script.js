cdf4047f3fd4edb6540f96cab016dc057237c56e
Fadhili <fadhoturi01@gmail.com>
2020-04-15 23:33:23 +0300

    Update function to show and hide elements in the page

--- js/script.js
+++ js/script.js
@@ -1,6 +1,14 @@
 $(document).ready(function() {
-    $(".design-clickable").click(function() {
-        $(".design-shown").slideToggle();
-        $(".design-hidden").slideToggle();
+    $(".des-clickable").click(function() {
+        $("#des-hidden").slideToggle();
+        $("#des-shown").slideToggle();
+    });
+    $(".dev-clickable").click(function() {
+        $("#dev-shown").slideToggle();
+        $("#dev-hidden").slideToggle();
+    });
+    $(".prod-clickable").click(function() {
+        $("#prod-shown").slideToggle();
+        $("#prod-hidden").slideToggle();
     });
 });
